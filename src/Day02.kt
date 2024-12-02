1fe72cd606c37c25c9292069439d47b324976bfb
Posac <mikio90@gmail.com>
2024-12-02 19:47:38 +0100

    Day-02- bit ugly

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,5 +1,4 @@
 import kotlin.math.abs
-import kotlin.math.sign
 
 private const val DAY_NAME = "Day02"
 fun main() {
@@ -14,15 +13,8 @@ fun main() {
 
 
 private fun part1(input: List<String>): Long {
-    return parse(input).map {
-        it.zipWithNext { prev, next ->
-            next - prev
-        }
-    }.filter {
-        val increasing = it.all { it > 0 }
-        val decreasing = it.all { it < 0 }
-        it.all { abs(it) <= 3 } && (increasing || decreasing)
-
+    return parse(input).filter {
+        checkList(it, thresholdProblem = 0).println("Results for list ${it}")
     }.size.toLong()
 }
 
@@ -37,21 +29,30 @@ private fun parse(input: List<String>) = input.map {
 
 private fun checkPart1() {
     check(part1(readInputResources(DAY_NAME, "test")).println("Part one test result") == 2L)
+    check(part1(readInputResources(DAY_NAME, "input")).println("Part one result") == 314L)
 }
 
 private fun checkPart2() {
-    check(part2(readInputResources(DAY_NAME, "test")).println("Part two test result") == 4L)
     check(part2(readInputResources(DAY_NAME, "test_mp")).println("Part two test_mp result") == 2L)
+    check(part2(readInputResources(DAY_NAME, "test")).println("Part two test result") == 4L)
     val result2 = part2(readInputResources(DAY_NAME, "input")).println("Part two test result")
-    check( result2 > 351L)
-    check( result2 < 412L)
+    check(result2 > 351L)
+    check(result2 != 408L)
+    check(result2 != 407L)
+    check(result2 < 412L)
+    check(result2 == 373L)
 }
 
 private fun part2(input: List<String>): Long = parse(input).filter {
     checkList(it).println("Results for list ${it}")
 }.size.toLong()
 
-private fun checkList(list: List<Long>, increasing: Boolean? = null, thresholdProblem: Int = 1, iteration : Int = 0): Boolean {
+private fun checkList(
+    list: List<Long>,
+    increasing: Boolean? = null,
+    thresholdProblem: Int = 1,
+    iteration: Int = 0
+): Boolean {
     println("[checkList][$iteration] increasing=$increasing thresholdProblem=$thresholdProblem list=$list")
     if (list.size < 2)
         return true
@@ -60,7 +61,11 @@ private fun checkList(list: List<Long>, increasing: Boolean? = null, thresholdPr
         val second = list[1]
         val distance = second - first
 
-        return if (increasing == null || thresholdProblem != 0) true else (if (increasing) distance > 0 else distance < 0)
+        return if (increasing == null || thresholdProblem != 0)
+            true
+        else ((if (increasing) distance > 0 else distance < 0) && isValidDistance(
+            distance
+        ))
     }
     val first = list[0]
     val second = list[1]
@@ -68,28 +73,61 @@ private fun checkList(list: List<Long>, increasing: Boolean? = null, thresholdPr
     val distanceOneTwo = second - first
     val distanceTwoThree = third - second
 
-    var newIcreasing = calculateIncreasing(increasing, distanceOneTwo)
+    val newIcreasing = calculateIncreasing(increasing, distanceOneTwo)
+    val firstPairIncreasing = distanceOneTwo > 0
+    val secondPairIncreasing = distanceTwoThree > 0
 
-    if(iteration == 0 &&!setOf(1, 2, 3L).contains(abs(distanceOneTwo))){
-        return checkList(
-            listOf(first) + list.drop(2),
-            increasing,
-            thresholdProblem - 1,
-            iteration=iteration+1
-        ) || checkList(listOf(second) + list.drop(2), increasing, thresholdProblem - 1,iteration=iteration+1)
-    } else if (  !setOf(1, 2, 3L).contains(abs(distanceTwoThree))) {
+    if (iteration == 0 && (!isValidDistance(distanceOneTwo) || !isValidIncrease(
+            firstPairIncreasing,
+            secondPairIncreasing,
+            increasing
+        ))
+    ) {
+
+        if (thresholdProblem != 0) {
+            return checkList(
+                list = listOf(first) + list.drop(2),
+                increasing = increasing,
+                thresholdProblem = thresholdProblem - 1,
+                iteration = iteration
+            ) || checkList(
+                list = listOf(second) + list.drop(2),
+                increasing = increasing,
+                thresholdProblem = thresholdProblem - 1,
+                iteration = iteration
+            )
+        } else return false
+    } else if ((!isValidDistance(distanceOneTwo) || !isValidDistance(distanceTwoThree) || !isValidIncrease(
+            firstPairIncreasing,
+            secondPairIncreasing,
+            increasing
+        ))
+    ) {
         if (thresholdProblem != 0)
             return checkList(
-                listOf(first,second) + list.drop(3),
-                increasing,
-                thresholdProblem - 1,
-                iteration=iteration+1
-            ) || checkList(listOf(first) + list.drop(2), increasing, thresholdProblem - 1,iteration=iteration+1)
+                list = listOf(first, second) + list.drop(3),
+                increasing = newIcreasing,
+                thresholdProblem = thresholdProblem - 1,
+                iteration = iteration + 1
+            ) || checkList(
+                list = listOf(first) + list.drop(2),
+                increasing = newIcreasing,
+                thresholdProblem = thresholdProblem - 1,
+                iteration = iteration + 1
+            )
         else return false
     } else
-        return checkList(list.drop(1), newIcreasing, thresholdProblem,iteration=iteration+1)
+        return checkList(
+            list = list.drop(1),
+            increasing = newIcreasing,
+            thresholdProblem = thresholdProblem,
+            iteration = iteration + 1
+        )
 
 }
+
+private fun isValidIncrease(firstPair: Boolean, secondPair: Boolean, increasing: Boolean?) =
+    if (increasing == null) firstPair == secondPair else (secondPair == increasing) && (firstPair == increasing)
 
 private fun calculateIncreasing(increasing: Boolean?, distanceOneTwo: Long): Boolean? {
     var newIcreasing = increasing
@@ -103,18 +141,7 @@ private fun calculateIncreasing(increasing: Boolean?, distanceOneTwo: Long): Boo
     return newIcreasing
 }
 
-private fun isSafety(
-    levelDiffs: Int,
-    it: List<Long>,
-    increasing: Int,
-    decreasing: Int
-) = levelDiffs == it.size && (increasing == it.size || decreasing == it.size)
-
-private fun calculateSafetyStatiscits(it: List<Long>): Triple<Int, Int, Int> {
-    val increasing = it.count { it > 0 }
-    val decreasing = it.count { it < 0 }
-    val levelDiffs = it.count { setOf(1, 2, 3L).contains(abs(it)) }
-    return Triple(increasing, decreasing, levelDiffs)
-}
+val VALID_DISTANCE = setOf(1, 2, 3L)
+private fun isValidDistance(distanceOneTwo: Long) = VALID_DISTANCE.contains(abs(distanceOneTwo))
 
 
